9d77df655b8ee0b4a47b1de4408554cfdfe54909
Swatar Common <swatarcommon@gmail.com>
2020-07-30 16:59:02 +0300

    Completed MVVM_architecture task and Readme was added

---
+++ MVVM_architecture/MVVM_architecture/Phone.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_architecture
+{
+    class Phone : INotifyPropertyChanged
+    {
+        private int id;
+        private string first_name;
+        private int phone_number;
+
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string First_Name
+        {
+            get { return first_name; }
+            set
+            {
+                first_name = value;
+                OnPropertyChanged("First_Name");
+            }
+        }
+        
+        public int Phone_Number
+        {
+            get { return phone_number; }
+            set
+            {
+                phone_number = value;
+                OnPropertyChanged("Phone_Number");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ MVVM_architecture/MVVM_architecture/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM_architecture
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Id=1, First_Name="Nikita", Phone_Number=6401337},
+                new Phone { Id=2, First_Name="Artemiy", Phone_Number=2281337},
+                new Phone { Id=3, First_Name="Ilya", Phone_Number=3221337}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
